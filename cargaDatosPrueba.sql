dc1b698c532552b0473f64926a63152a50fad62f
Leonel Morsella <leonelmorsella@hotmail.com>
2022-10-29 16:32:40 -0300

    se carga scripts SQL de prueba

---
+++ cargaDatosPrueba.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Hannibal', 'Lecter', 'Dragon Rojo')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Will', 'Graham', 'Dragon Rojo')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Freddie', 'Lounds', 'Dragon Rojo')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Isaac', 'Asimov', 'La Fundacion')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Dio', 'Brando', 'Jojo')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Jotaro', 'Kujo', 'Jojo')
+INSERT INTO persona(nombre,apellido,libroFavorito) VALUES ('Shinji', 'Akari', 'Evangelion')
